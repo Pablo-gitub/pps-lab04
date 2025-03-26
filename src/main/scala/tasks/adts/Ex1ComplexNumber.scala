8483fb3a3adc8e73a28e2902995e26f10aef1272
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-26 05:22:59 +0100

    task1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,7 @@
 package tasks.adts
 
+import java.util.Locale
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -18,13 +20,17 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
-
+    case class ComplexImpl(re: Double, im: Double)
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = ComplexImpl
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = ComplexImpl(complex.re + other.re, complex.im + other.im)
+      def subtract(other: Complex): Complex = ComplexImpl(complex.re - other.re, complex.im - other.im)
+      def asString(): String = complex match
+        case ComplexImpl(_, 0) => String.format(Locale.US, "%.1f", java.lang.Double.valueOf(complex.re))
+        case ComplexImpl(0, _) => String.format(Locale.US, "%.1fi", java.lang.Double.valueOf(complex.im))
+        case ComplexImpl(re, im) if im < 0  => String.format(Locale.US, "%.1f - %.1fi", java.lang.Double.valueOf(complex.re), math.abs(java.lang.Double.valueOf(complex.im)))
+        case _ => String.format(Locale.US, "%.1f + %.1fi", java.lang.Double.valueOf(complex.re), java.lang.Double.valueOf(complex.im))

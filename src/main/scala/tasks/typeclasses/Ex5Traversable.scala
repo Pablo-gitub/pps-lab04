3a18fb8664f5369c72cd30f4bb0c28b9941a2978
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-27 01:58:51 +0100

    task5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,10 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.Optional
+import u03.Optionals.Optional.*
+
+import scala.annotation.tailrec
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +21,56 @@ import Sequence.*
 
 object Ex5Traversable:
 
+  trait Traversable[T[_]]:
+    def traverse[A](ta: T[A])(f: A => Unit): Unit
+
+  given Traversable[Sequence] with
+    def traverse[A](seq: Sequence[A])(f: A => Unit): Unit = seq match
+      case Cons(h, t) =>
+        f(h)
+        traverse(t)(f)
+      case Nil() => ()
+
+  given Traversable[Optional] with
+    def traverse[A](opt: Optional[A])(f: A => Unit): Unit = opt match
+      case Just(a) => f(a)
+      case Empty() => ()
+
   def log[A](a: A): Unit = println("The next element is: "+a)
 
+  @tailrec
   def logAll[A](seq: Sequence[A]): Unit = seq match
     case Cons(h, t) => log(h); logAll(t)
     case _ => ()
 
-  
+  def logAllGen[T[_] : Traversable, A](ta: T[A])(log: A => Unit): Unit =
+    summon[Traversable[T]].traverse(ta)(log)
+
+  def printAll[T[_] : Traversable, A](ta: T[A]): Unit =
+    logAllGen(ta)(println)
+
+@main def tryLogAll(): Unit =
+
+  import Ex5Traversable.*
+  import u03.Optionals.Optional.*
+
+  // Test with Sequence:
+  val seq: Sequence[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+  println("LogAllGen on Sequence using log:")
+  logAllGen(seq)(log)
+  println("PrintAll on Sequence using println:")
+  printAll(seq)
+
+  // Test with Optional:
+  val opt1: Optional[String] = Just("Hello")
+  val opt2: Optional[String] = Empty()
+
+  println("LogAllGen on Optional (Just):")
+  logAllGen(opt1)(log)
+  println("PrintAll on Optional (Just):")
+  printAll(opt1)
+
+  println("LogAllGen on Optional (Empty):")
+  logAllGen(opt2)(log)
+  println("PrintAll on Optional (Empty):")
+  printAll(opt2)
